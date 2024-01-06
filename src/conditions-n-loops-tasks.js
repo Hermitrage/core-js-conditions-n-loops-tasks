46ea752ae1e167545aacf1b83da5028ef3f381ee
Hermitrage <twrichsilent@gmail.com>
2024-01-06 15:11:00 +0300

    fix: solve problem

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -510,8 +510,28 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const random = arr[0];
+  const left = [];
+  const right = [];
+  for (let i = 1; i < arr.length; i += 1) {
+    if (random > arr[i]) {
+      left[left.length] = arr[i];
+    } else {
+      right[right.length] = arr[i];
+    }
+  }
+
+  const result = [...sortByAsc(left), random, ...sortByAsc(right)];
+  const copy = arr;
+  for (let i = 0; i < result.length; i += 1) {
+    copy[i] = result[i];
+  }
+
+  return copy;
 }
 
 /**
